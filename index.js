c924b9753a6390b764b27e9882cfd3e55ecd8b93
DArtangnon <dartangnon101@hotmail.com>
2025-05-03 11:41:31 -0700

    Finalized Project

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
+// GRABBING ELEMENTS FROM HTML
 const moviesWrapper = document.querySelector('.movies');
-console.log(moviesWrapper)
+const searchName = document.querySelector('.search__name');
 
+// GLOBAL MOVIE VARIABLE
+let currentMovies = []
+
+// HANDLING THE SEARCH
 function searchChange(event) {
-    console.log(event.target.value);
+    renderMovies(event.target.value);
+    searchName.innerHTML = event.target.value;
 }
 
-
-
-async function renderMovies() {
- const response = await fetch(`https://omdbapi.com/?s=batman&apikey=3be48169`);
+// RENDERING MOVIES / CALLING API
+async function renderMovies(searchTerm) {
+ const response = await fetch(`https://omdbapi.com/?s=${searchTerm}&apikey=3be48169`);
  const data = await response.json();
- const moviesArr = data.Search;
-
+ currentMovies = data.Search;
+ displayMovies(currentMovies);
 }
 
-setTimeout(() => {
-    renderMovies();
-});
-    
+
+// DISPLAYING MOVIES
+function displayMovies(movieList) {
+moviesWrapper.innerHTML = movieList.map((movie) => {
+    return `
+    <div class="movie">
+    <figure class="img__Wrapper">
+        <img class="image" src="${movie.Poster}" alt="">
+        </figure>
+        <h2>${movie.Title}</h2>
+        <h4>${movie.Year}</h4>
+        <button class="learn__btn">Learn More</button>
+</div>
+    `
+})
+.join("");
+}
+
+// FILTERING MOVIES
+function filterMovies(event) {
+const filterOption = event.target.value;
+
+let filteredMovies = [...currentMovies];
+
+if (filterOption === "newest") {
+    filteredMovies.sort((a, b) => b.Year - a.Year);
+    } 
+else if (filterOption === "oldest") {
+    filteredMovies.sort((a, b) => a.Year - b.Year);
+}
+
+displayMovies(filteredMovies);
+}
